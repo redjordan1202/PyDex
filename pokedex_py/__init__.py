9040bfe5fd8e17dbf0567f6ba519007fdf9f9dd6
redjordan1202 <75704668+redjordan1202@users.noreply.github.com>
2022-05-12 16:38:23 -0700

    feat(Pip Packaging): Made changes to allow for pip packaging

---
+++ pokedex_py/__init__.py
@@ -0,0 +1,9 @@
+'''
+
+pokedex_py
+
+A python wrapper for the PokéAPI RESTful api
+'''
+
+__version__ = "1.0"
+__author__ = 'Jordan Del Pilar'
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pokedex_py',
+    version='1.1',    
+    description='Python wrapper for the PokéAPI RESTful api',
+    url='https://github.com/redjordan1202/PyDex',
+    author='Jordan Del Pilar',
+    author_email='redjordan123@gmail.com',
+    license='MIT',
+    packages=['pokedex_py'],
+    install_requires=['requests'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+)
